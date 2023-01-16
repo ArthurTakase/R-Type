5e232cfe38c071e33aa2a4d343e162776ddbc689
Solène Lefeu <solene.lefeu@epitech.eu>
2023-01-16 18:33:05 +0100

    UPD: try and catch in main function

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,18 @@
 #include <memory>
 
 #include "Entity.hpp"
+#include "Error.hpp"
 #include "HitboxComponent.hpp"
 #include "PositionComponent.hpp"
 #include "RType.hpp"
 
 int main()
 {
-    std::unique_ptr<RType> rtype = std::make_unique<RType>();
-    return 0;
+    try {
+        std::unique_ptr<RType> rtype = std::make_unique<RType>();
+    } catch (Error const& error) {
+        std::cerr << error.what() << std::endl;
+        return (84);
+    }
+    return (0);
 }
