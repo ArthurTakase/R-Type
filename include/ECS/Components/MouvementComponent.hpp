da7b379d2572adcd5f0bf6a575f7b70a993cfc02
guillaume <guillaume.vernizeau@epitech.eu>
2023-01-16 22:13:34 +0100

    ADD: Mouvement Component

---
+++ include/ECS/Components/MouvementComponent.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2022
+** YEP
+** File description:
+** TransformComponent.cpp
+*/
+
+#pragma once
+
+#include "IComponent.hpp"
+
+/**
+ * @brief Component used to set the direction and speed of the movement of an entity
+ */
+
+class MouvementComponent : public IComponent
+{
+    public:
+        MouvementComponent(float dirX, float dirY, float speed);
+        ~MouvementComponent() noexcept;
+        MouvementComponent(const MouvementComponent& other) noexcept = default;
+        MouvementComponent(MouvementComponent&& other) noexcept      = delete;
+
+        MouvementComponent& operator=(const MouvementComponent& rhs) noexcept = delete;
+        MouvementComponent& operator=(MouvementComponent&& rhs) noexcept      = delete;
+
+        float getDirX() const noexcept;
+        float getDirY() const noexcept;
+        float getSpeed() const noexcept;
+        void setDirX(float x) noexcept;
+        void setDirY(float y) noexcept;
+        void setSpeed(float speed) noexcept;
+
+    private:
+        float _dirX;
+        float _dirY;
+        float _speed;
+};
