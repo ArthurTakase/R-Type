00a36dc3399cb53d275fcd3b072a74a9cc5d55dc
arthur <arthur.decaen@epitech.eu>
2023-01-18 11:44:45 +0100

    UP: format

--- src/ECS/Components/StatComponent.cpp
+++ src/ECS/Components/StatComponent.cpp
@@ -11,7 +11,7 @@
 
 /**
  * It's a constructor that takes two parameters and assigns them to the two member variables
- * 
+ *
  * @param life The life of the entity.
  * @param damage The damage of the entity.
  */
@@ -28,7 +28,7 @@ StatComponent::~StatComponent() noexcept {}
 
 /**
  * It returns the value of the private member _life
- * 
+ *
  * @return The life of the entity.
  */
 int StatComponent::getLife() const noexcept
@@ -38,7 +38,7 @@ int StatComponent::getLife() const noexcept
 
 /**
  * It returns the value of the private attribute _damage
- * 
+ *
  * @return The damage of the entity.
  */
 int StatComponent::getDamage() const noexcept
@@ -48,7 +48,7 @@ int StatComponent::getDamage() const noexcept
 
 /**
  * It sets the value of the private member _life
- * 
+ *
  * @param life The life of the entity.
  */
 void StatComponent::setLife(int life) noexcept
@@ -58,7 +58,7 @@ void StatComponent::setLife(int life) noexcept
 
 /**
  * It sets the value of the private attribute _damage
- * 
+ *
  * @param damage The damage of the entity.
  */
 void StatComponent::setDamage(int damage) noexcept
