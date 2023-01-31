eb5b930c1b818fd3e31d65db17027243518249a7
roy-corentin <corentin.roy@epitech.eu>
2023-01-31 11:51:27 +0100

    ADD: serialize test in main

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <memory>
 
-#include "Entity.hpp"
+#include "DrawableComponent.hpp"
 #include "HitboxComponent.hpp"
 #include "Lib.hpp"
 #include "RType.hpp"
@@ -18,7 +18,16 @@ int main()
 {
     auto lib = std::make_unique<Lib>();
     lib->getWindow().open(800, 600, "R-Type");
-    auto& window = lib->getWindow();
+    auto& window     = lib->getWindow();
+    auto  entity     = std::make_unique<Entity>(0);
+    auto  transform  = TransformComponent(234, 69);
+    auto  drawable   = DrawableComponent(23, 32, 140, 220, 3);
+    auto  serializer = std::make_unique<SerializerSystem>();
+
+    entity->addComponent(transform);
+    entity->addComponent(drawable);
+    auto data = serializer->Serialize(entity);
+    std::cout << data << std::endl;
 
     while (window.isOpen()) {
         window.clear();
