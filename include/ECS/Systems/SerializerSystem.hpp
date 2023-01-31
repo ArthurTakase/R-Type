4efd3c6ee80f80cf0b2e039000845a1a9bda644f
roy-corentin <corentin.roy@epitech.eu>
2023-01-31 11:46:03 +0100

    ADD: function to serialize entity and key code, UP: some code

---
+++ include/ECS/Systems/SerializerSystem.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <bitset>
+#include <memory>
+
+#include "DrawableComponent.hpp"
+#include "EntityIterator.hpp"
+#include "TransformComponent.hpp"
+
+/**
+ * @brief Macros for Element's BIT size
+ */
+namespace Serializer
+{
+#define ENTITYSIZE 88
+#define XSIZE 16
+#define YSIZE 16
+#define IDSPRiTESIZE 8
+#define WIDTHSIZE 8
+#define HEIGHTSIZE 8
+#define SCALEXSIZE 8
+#define SCALEYSIZE 8
+#define OFFSETXSIZE 8
+#define OFFSETYSIZE 8
+
+#define INPUTSIZE 8
+};
+
+/**
+ * @brief Object used to serialize Entity for network
+ */
+class SerializerSystem
+{
+  public:
+    //! Default constructor
+    SerializerSystem();
+    virtual ~SerializerSystem() noexcept = default;
+
+    SerializerSystem(const SerializerSystem& other)                = delete;
+    SerializerSystem(SerializerSystem&& other) noexcept            = delete;
+    SerializerSystem& operator=(const SerializerSystem& other)     = delete;
+    SerializerSystem& operator=(SerializerSystem&& other) noexcept = delete;
+
+    std::bitset<ENTITYSIZE> Serialize(std::unique_ptr<Entity> const& entity) const noexcept;
+    std::bitset<INPUTSIZE>  Serialize(int& keyCode) const noexcept;
+
+  protected:
+  private:
+    std::unique_ptr<EntityIterator<TransformComponent, DrawableComponent>> _it;
+};
