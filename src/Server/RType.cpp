1a0efd96fddfe98d7fec70a054c97a12c1e7645f
arthur <arthur.decaen@epitech.eu>
2023-02-20 12:02:55 +0100

    FIX: remove useless tests

--- src/Server/RType.cpp
+++ src/Server/RType.cpp
@@ -57,9 +57,9 @@ void RType::init() noexcept
 {
     createBackground(0);
     createBackground(255);
-    // createAsteroid(255);
-    // createAsteroid(300);
-    // createAsteroid(200);
+    createAsteroid(255);
+    createAsteroid(300);
+    createAsteroid(200);
     createEnemy(255, 120);
     createEnemy(300, 200);
     createEnemy(200, 50);
